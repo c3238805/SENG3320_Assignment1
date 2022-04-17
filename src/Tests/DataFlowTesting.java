c40006d4bb0e69992b405c91b7b2280fc0b05634
ni <c3238805@uon.edu.au>
2022-04-17 18:54:26 +1000

    DataFlow update

--- src/Tests/DataFlowTesting.java
+++ src/Tests/DataFlowTesting.java
@@ -19,18 +19,93 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * 
  */
 public class DataFlowTesting {
+
     @Test
-    void gcdTest() {
+    void gcdT1() {
+        // below test method is to test public BigInteger gcd(BigInteger y)
+
+        // test case t1: (xval = -8, yval = -12)
+
+        BigInteger x = new BigInteger("-8");
+        BigInteger y = new BigInteger("-12" );
+
+        assertEquals(new BigInteger("4"),x.gcd(y));
+    }
+
+    @Test
+    void gcdT2() {
         // this test method is to test public BigInteger gcd(BigInteger y)
 
-        
+        // test case t1: (xval = 1, yval = 0)
         BigInteger x = new BigInteger("1");
-        BigInteger y = new BigInteger("1" );
-        BigInteger result = x.add(y);
+        BigInteger y = new BigInteger("0");
 
-        assertEquals(2,x.gcd);
-
-
+        assertEquals(new BigInteger("1"), x.gcd(y));
 
     }
+
+    @Test
+    void gcdT3() {
+        // this test method is to test public BigInteger gcd(BigInteger y)
+
+        // test case t1: (xval = -2, yval = 0)
+        BigInteger x = new BigInteger("-2");
+        BigInteger y = new BigInteger("0");
+
+        assertEquals(new BigInteger("2"), x.gcd(y));
+
+    }
+    
+    @Test
+    void gcdT4() {
+        // this test method is to test public BigInteger gcd(BigInteger y)
+
+        // test case t4: (xval = 3, yval = -9)
+        BigInteger x = new BigInteger("-3");
+        BigInteger y = new BigInteger("-9");
+
+        assertEquals(new BigInteger("3"), x.gcd(y));
+
+    }
+
+    @Test
+    void gcdT5() {
+        // this test method is to test public BigInteger gcd(BigInteger y)
+
+        // test case t5: (xval = Integer.MIN_VALUE, yval = 0)
+        
+        BigInteger x = new BigInteger("-21474836481");
+        BigInteger y = new BigInteger("0");
+
+        assertEquals(new BigInteger("21474836481"), x.gcd(y));
+
+    }
+
+    @Test
+    void gcdT6() {
+        // this test method is to test public BigInteger gcd(BigInteger y)
+
+        // test case t6: (xval = 100, yval = 0)
+
+        BigInteger x = new BigInteger("100");
+        BigInteger y = new BigInteger("0");
+
+        assertEquals(new BigInteger("100"), x.gcd(y));
+
+    }
+
+    @Test
+    void gcdT7() {
+        // this test method is to test public BigInteger gcd(BigInteger y)
+
+        // test case t7: (xval = 2386, yval = 21474836481F)
+
+        BigInteger x = new BigInteger("2386");
+        BigInteger y = new BigInteger("21474836481F", 16);
+
+        System.out.println(y);
+        assertEquals(new BigInteger("1"), x.gcd(y));
+
+    }
+
 }
